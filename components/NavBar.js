4c2257802b4fbb28e43d89344280a1a11ecc2b19
Naman Sharma <159528188+Namansharma1327@users.noreply.github.com>
2024-09-06 00:31:51 +0530

    Add files via upload

---
+++ components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <div className='navbar'>
+        <nav>
+            <p>i am navBar</p>
+        </nav>
+      
+    </div>
+  )
+}
+
+export default NavBar
